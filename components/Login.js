7499122cc0ce46f719a5f24f12eb2a79058f15dd
YeonSeong-Lee <dltjddus3@gmail.com>
2021-11-07 03:14:33 +0900

    create conponents!

---
+++ components/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet, Text, View, Dimensions, ScrollView } from "react-native";
+
+export default function Login() {
+  return;
+}
+
+const styles = StyleSheet.create({});
