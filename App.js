5eeae2930d621a39c5bfd01ae9b0630668269d03
GyuhaWang <86870218+GyuhaWang@users.noreply.github.com>
2021-11-09 18:01:03 +0900

    Update App.js
    midde 의 list를 usestate써서 jason 으로 저장하기,   // map 쓰는데  Image 부분source  속에 listdata.icon 어떡할지 모르겠음

--- App.js
+++ App.js
@@ -1,43 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View,Dimensions,ScrollView,Image,useState} from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View,Dimensions,ScrollView,Image, Pressable} from 'react-native';
 const {width: SCREEN_WIDTH } = Dimensions.get("window");
 const {height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 
 export default function App() {
- 
+ const [listData,setlistDate] = useState([
+   {
+     icon:'https://raw.githubusercontent.com/NOON-official/deliveryParty/main/assets/icon.png',
+     participant:1,
+     Title:"떡볶이 시키실 ",
+     description:"떡복이 떡볶이",
+     name:"A동 귀여운 호랑이",
+     time:"17:00",
+     key:'1'
+   }
+ ]);
   return (
     <View style={styles.container}>
       <View style={styles.mainTop}>
         <Text style={{fontSize: 35,textAlign:'left',paddingLeft:30,paddingTop:25}}>오늘의 파티</Text>
       </View>
       <View style={styles.mainMiddle}>
-      
-      <ScrollView contentContainerStyle={{flex:1}}>
+      {listData.map((item)=>{
+        return(
+     <ScrollView contentContainerStyle={{flex:1}}  key={item.key}>
       <View  style={styles.list}>
-      <View style={{flex:1,marginBottom:30,marginTop:30}}>
-      <Image style={{flex:3}} source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',}}/>   
-      <Text style={{flex:1,}}> 여기는 숫자</Text> 
-      </View>
-      <View style={{flex:3,marginTop:30,marginBottom:30,marginLeft:10}}>
-      <Text style={{flex:1,fontSize:20}}>떡볶이 시키실 분?</Text>  
-      <Text style={{flex:1, fontSize:15}}>떡볶이 떡볶이</Text>
+      <Pressable style={{flex:1,marginBottom:30,marginTop:30}}>
+      <Image style={{flex:3}} source={{uri:'https://raw.githubusercontent.com/NOON-official/deliveryParty/main/assets/icon.png',}}/>   
+      <Text style={{flex:1,textAlign:'center',marginTop:5}}> 1/5</Text> 
+      </Pressable>
+      <Pressable style={{flex:3,marginTop:30,marginBottom:30,marginLeft:10}}>
+      <Text style={{flex:1,fontSize:20}}>{item.Title}</Text>  
+      <Text style={{flex:1, fontSize:15}}>{item.description}</Text>
       <View style={{flexDirection:'row',flex:1,}}>
-      <Text style={{fontSize:15,borderStyle:'solid',borderRightWidth:10,}}>A동 귀여운 호랑이</Text>
-      <Text style={{fontSize:15}}>17:00</Text>
+      <Text style={{fontSize:15,borderRightColor:'black',borderRightWidth:1}}>{item.name}</Text>
+      <Text style={{fontSize:15, marginLeft:5}}>{item.time}</Text>
       </View>
+      </Pressable>
       </View>
-      </View>
-      
-
-
-
       </ScrollView>
-      </View>
-       
-      <View style={styles.mainBottom}>
-        <Text></Text>
+      
+      )})}
       </View>
       <StatusBar style="auto" />
       
@@ -67,7 +72,10 @@ const styles = StyleSheet.create({
   mainBottom :{
     flex: 1,
     backgroundColor: "white",
-    width: SCREEN_WIDTH
+    width: SCREEN_WIDTH,
+    borderStyle:'solid',
+    borderWidth:1,
+    flexDirection:'row'
   },
   text :{
 color : "red",
@@ -80,12 +88,14 @@ alignItems: 'center'
       alignItems: "center",
       alignItems: "flex-start",
       paddingHorizontal: 20,
-      backgroundColor:"red",
+      backgroundColor:"white",
       marginLeft: 10,
       marginRight: 10,
       marginTop:5,
       marginBottom:10,
-      flexDirection:'row'
+      flexDirection:'row',
+      borderBottomColor: 'black',
+      borderBottomWidth:1
   },
   listImage:{
   flex:1,
