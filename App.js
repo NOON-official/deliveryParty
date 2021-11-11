41057579e91b97661b0b855bb4fdee69089011b0
YeonSeong-Lee <dltjddus3@gmail.com>
2021-11-12 02:40:53 +0900

    phone login

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MainPage from "./components/MainPage";
+import Login from "./components/Login";
 
 export default function App() {
-  return <MainPage />;
+  return <Login />;
 }
